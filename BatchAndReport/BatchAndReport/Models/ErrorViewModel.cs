17885c289a14698cff3e07fffee3e0b54681cfb0
osmepSI2025 <osmep.si2025@gmail.com>
2025-05-28 17:47:21 +0700

    delete

--- BatchAndReport/BatchAndReport/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BatchAndReport.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
