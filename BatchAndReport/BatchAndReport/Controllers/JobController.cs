b48067edff3633b3846acb41c64e8325dbe38c0c
SukanyaDoubleP <sukanya@double-p.co.th>
2025-06-05 21:12:41 +0700

    test

--- BatchAndReport/BatchAndReport/Controllers/JobController.cs
+++ BatchAndReport/BatchAndReport/Controllers/JobController.cs
@@ -1,7 +1,10 @@
-﻿using BatchAndReport.Models;
+﻿using BatchAndReport.Entities;
+using BatchAndReport.Models;
 using BatchAndReport.Repository;
 using BatchAndReport.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
 
 namespace BatchAndReport.Controllers
 {
@@ -12,11 +15,13 @@ namespace BatchAndReport.Controllers
         private readonly ScheduledJobService _jobService;
         private readonly IApiInformationRepository _repositoryApi;
         private readonly ICallAPIService _serviceApi;
-        public JobController(ScheduledJobService jobService, IApiInformationRepository repositoryApi, ICallAPIService serviceApi)
+        private readonly K2DBContext _k2context;
+        public JobController(ScheduledJobService jobService, IApiInformationRepository repositoryApi, ICallAPIService serviceApi, K2DBContext k2context)
         {
             _jobService = jobService;
             _repositoryApi = repositoryApi;
             _serviceApi = serviceApi;
+            _k2context = k2context;
         }
 
         [HttpPost("run")]
@@ -30,32 +35,118 @@ namespace BatchAndReport.Controllers
         [HttpPost("GetEmpHR")]
         public async Task<IActionResult> GetEmpHR([FromQuery] int page, int perPage)
         {
-            var smodel = new searchEmployeeModels
+            try
             {
-                page = page,
-                perPage = perPage
-            };
-            var LApi = await _repositoryApi.GetAllAsync(new MapiInformationModels { ServiceNameCode = "employee-all" });
-            var apiParam = LApi.Select(x => new MapiInformationModels
+                var smodel = new searchEmployeeModels
+                {
+                    page = page,
+                    perPage = perPage
+                };
+
+                var LApi = await _repositoryApi.GetAllAsync(new MapiInformationModels { ServiceNameCode = "employee-all" });
+                var apiParam = LApi.Select(x => new MapiInformationModels
+                {
+                    ServiceNameCode = x.ServiceNameCode,
+                    ApiKey = x.ApiKey,
+                    AuthorizationType = x.AuthorizationType,
+                    ContentType = x.ContentType,
+                    CreateDate = x.CreateDate,
+                    Id = x.Id,
+                    MethodType = x.MethodType,
+                    ServiceNameTh = x.ServiceNameTh,
+                    Urldevelopment = x.Urldevelopment,
+                    Urlproduction = x.Urlproduction,
+                    Username = x.Username,
+                    Password = x.Password,
+                    UpdateDate = x.UpdateDate
+                }).FirstOrDefault();
+
+                if (apiParam == null)
+                    return BadRequest("API info not found.");
+
+                var result = await _serviceApi.GetDataApiAsync(apiParam, smodel);
+
+                var employees = JsonSerializer.Deserialize<List<MEmployeeModels>>(result.ToString());
+
+                if (employees == null)
+                    return BadRequest("Cannot deserialize employee data");
+
+                await InsertOrUpdateEmployeesAsync(employees);
+
+                return Ok(new { message = "Sync and Save Complete", total = employees.Count });
+            }
+            catch (Exception ex)
             {
-                ServiceNameCode = x.ServiceNameCode,
-                ApiKey = x.ApiKey,
-                AuthorizationType = x.AuthorizationType,
-                ContentType = x.ContentType,
-                CreateDate = x.CreateDate,
-                Id = x.Id,
-                MethodType = x.MethodType,
-                ServiceNameTh = x.ServiceNameTh,
-                Urldevelopment = x.Urldevelopment,
-                Urlproduction = x.Urlproduction,
-                Username = x.Username,
-                Password = x.Password,
-                UpdateDate = x.UpdateDate
-            }).First(); // ดึงตัวแรกของ List
+                return StatusCode(500, new
+                {
+                    message = "Internal Server Error",
+                    error = ex.Message,
+                    inner = ex.InnerException?.Message,
+                    stack = ex.StackTrace
+                });
+            }
+        }
 
+        public async Task InsertOrUpdateEmployeesAsync(List<MEmployeeModels> employees)
+        {
+            foreach (var emp in employees)
+            {
+                var existingEmp = await _k2context.Employees
+                    .FirstOrDefaultAsync(e => e.EmployeeId == emp.EmployeeId);
 
-            var result = await _serviceApi.GetDataApiAsync(apiParam, smodel);
-            return Ok(result);
+                if (existingEmp != null)
+                {
+                    // UPDATE
+                    existingEmp.EmployeeCode = emp.EmployeeCode;
+                    existingEmp.NameTh = emp.NameTh;
+                    existingEmp.NameEn = emp.NameEn;
+                    existingEmp.FirstNameTh = emp.FirstNameTh;
+                    existingEmp.FirstNameEn = emp.FirstNameEn;
+                    existingEmp.LastNameTh = emp.LastNameTh;
+                    existingEmp.LastNameEn = emp.LastNameEn;
+                    existingEmp.Email = emp.Email;
+                    existingEmp.Mobile = emp.Mobile;
+                    existingEmp.EmploymentDate = emp.EmploymentDate;
+                    existingEmp.TerminationDate = emp.TerminationDate;
+                    existingEmp.EmployeeType = emp.EmployeeType;
+                    existingEmp.EmployeeStatus = emp.EmployeeStatus;
+                    existingEmp.SupervisorId = emp.SupervisorId;
+                    existingEmp.CompanyId = emp.CompanyId;
+                    existingEmp.BusinessUnitId = emp.BusinessUnitId;
+                    existingEmp.PositionId = emp.PositionId;
+
+                    _k2context.Employees.Update(existingEmp);
+                }
+                else
+                {
+                    // INSERT
+                    var newEmp = new Employee
+                    {
+                        EmployeeId = emp.EmployeeId,
+                        EmployeeCode = emp.EmployeeCode,
+                        NameTh = emp.NameTh,
+                        NameEn = emp.NameEn,
+                        FirstNameTh = emp.FirstNameTh,
+                        FirstNameEn = emp.FirstNameEn,
+                        LastNameTh = emp.LastNameTh,
+                        LastNameEn = emp.LastNameEn,
+                        Email = emp.Email,
+                        Mobile = emp.Mobile,
+                        EmploymentDate = emp.EmploymentDate,
+                        TerminationDate = emp.TerminationDate,
+                        EmployeeType = emp.EmployeeType,
+                        EmployeeStatus = emp.EmployeeStatus,
+                        SupervisorId = emp.SupervisorId,
+                        CompanyId = emp.CompanyId,
+                        BusinessUnitId = emp.BusinessUnitId,
+                        PositionId = emp.PositionId
+                    };
+
+                    await _k2context.Employees.AddAsync(newEmp);
+                }
+            }
+
+            await _k2context.SaveChangesAsync();
         }
     }
 }
---
+++ BatchAndReport/BatchAndReport/Entities/K2DBContext.cs
@@ -0,0 +1,131 @@
+﻿using BatchAndReport.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+namespace BatchAndReport.Entities;
+
+
+public partial class K2DBContext : DbContext
+{
+    public K2DBContext()
+    {
+    }
+
+    public K2DBContext(DbContextOptions<BatchDBContext> options)
+        : base(options)
+    {
+    }
+
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=doublep-cloud.servehttp.com;Database=K2_HR;User Id=Dev;Password=J01nSP0pD0ub!EP;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.ToTable("Employee");
+
+            entity.HasKey(e => e.Id);
+
+            entity.Property(e => e.Id)
+                .HasColumnName("Id");
+
+            entity.Property(e => e.EmployeeId)
+                .HasColumnName("EmployeeId")
+                .HasMaxLength(50)
+                .IsRequired()
+                .UseCollation("Thai_CI_AS");
+
+            entity.Property(e => e.EmployeeCode)
+                .HasColumnName("EmployeeCode")
+                .HasMaxLength(50)
+                .UseCollation("Thai_CI_AS");
+
+            entity.Property(e => e.NameTh)
+                .HasColumnName("NameTh")
+                .HasMaxLength(100)
+                .UseCollation("Thai_CI_AS");
+
+            entity.Property(e => e.NameEn)
+                .HasColumnName("NameEn")
+                .HasMaxLength(100)
+                .UseCollation("Thai_CI_AS");
+
+            entity.Property(e => e.FirstNameTh)
+                .HasColumnName("FirstNameTh")
+                .HasMaxLength(100)
+                .UseCollation("Thai_CI_AS");
+
+            entity.Property(e => e.FirstNameEn)
+                .HasColumnName("FirstNameEn")
+                .HasMaxLength(100)
+                .UseCollation("Thai_CI_AS");
+
+            entity.Property(e => e.LastNameTh)
+                .HasColumnName("LastNameTh")
+                .HasMaxLength(100)
+                .UseCollation("Thai_CI_AS");
+
+            entity.Property(e => e.LastNameEn)
+                .HasColumnName("LastNameEn")
+                .HasMaxLength(100)
+                .UseCollation("Thai_CI_AS");
+
+            entity.Property(e => e.Email)
+                .HasColumnName("Email")
+                .HasMaxLength(100)
+                .UseCollation("Thai_CI_AS");
+
+            entity.Property(e => e.Mobile)
+                .HasColumnName("Mobile")
+                .HasMaxLength(20)
+                .UseCollation("Thai_CI_AS");
+
+            entity.Property(e => e.EmploymentDate)
+                .HasColumnName("EmploymentDate");
+
+            entity.Property(e => e.TerminationDate)
+                .HasColumnName("TerminationDate");
+
+            entity.Property(e => e.EmployeeType)
+                .HasColumnName("EmployeeType")
+                .HasMaxLength(50)
+                .UseCollation("Thai_CI_AS");
+
+            entity.Property(e => e.EmployeeStatus)
+                .HasColumnName("EmployeeStatus")
+                .HasMaxLength(50)
+                .UseCollation("Thai_CI_AS");
+
+            entity.Property(e => e.SupervisorId)
+                .HasColumnName("SupervisorId")
+                .HasMaxLength(50)
+                .UseCollation("Thai_CI_AS");
+
+            entity.Property(e => e.CompanyId)
+                .HasColumnName("CompanyId")
+                .HasMaxLength(50)
+                .UseCollation("Thai_CI_AS");
+
+            entity.Property(e => e.BusinessUnitId)
+                .HasColumnName("BusinessUnitId")
+                .HasMaxLength(50)
+                .UseCollation("Thai_CI_AS");
+
+            entity.Property(e => e.PositionId)
+                .HasColumnName("PositionId")
+                .HasMaxLength(50)
+                .UseCollation("Thai_CI_AS");
+        });
+
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
