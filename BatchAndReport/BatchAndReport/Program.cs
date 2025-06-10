9df32c77da74e4438f543c966d3ee434ba7f2826
osmepSI2025 <osmep.si2025@gmail.com>
2025-06-10 20:18:20 +0700

    up caode

--- BatchAndReport/BatchAndReport/Program.cs
+++ BatchAndReport/BatchAndReport/Program.cs
@@ -35,14 +35,20 @@ builder.Services.AddDbContext<K2DBContext>(options =>
 
 // Inside the builder.Services section:
 builder.Services.AddDbContext<K2DBContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("K2DBContext_EContract")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("K2DBContext")));
 
+builder.Services.AddDbContext<K2DBContext_SME>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("K2DBContext_SME")));
+builder.Services.AddDbContext<K2DBContext_EContract>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("K2DBContext_EContract")));
 
 // Register ScheduledJobService as a singleton and hosted service
 builder.Services.AddScoped<IApiInformationRepository, ApiInformationRepository>();
 builder.Services.AddScoped<ICallAPIService, CallAPIService>();
 builder.Services.AddScoped<SqlConnectionDAO>();
 builder.Services.AddScoped<HrDAO>();
+builder.Services.AddScoped<SmeDAO>();
+builder.Services.AddScoped<EContractDAO>();
 builder.Services.AddHttpClient<ICallAPIService, CallAPIService>();
 
 builder.Services.AddSingleton<ScheduledJobService>();
@@ -56,12 +62,13 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
-else
+else 
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+  
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
